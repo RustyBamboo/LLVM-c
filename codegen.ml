c493c2a86cfd2dda804fb3c7cfa2cd193d95ad00
RustyBamboo <dvolya@gmail.com>
2019-05-01 20:00:01 -0400

    now runs

--- codegen.ml
+++ codegen.ml
@@ -1,4 +1,5 @@
 open Llvm
+open Expr
 
 
 exception Error of string
@@ -8,18 +9,19 @@ let the_module = create_module context "my cool jit"
 let builder = builder context
 let named_values:(string, llvalue) Hashtbl.t = Hashtbl.create 10
 let double_type = double_type context
+let int_type = i32_type context
 
-let codegen_expr (e: Expr) =
+let rec codegen_expr (e: expr) =
     match e with
-    | Int n -> const_float int_type n
+    | Int n -> const_int int_type n
     | Double n -> const_float double_type n
     | MethodCall (callee, args) -> 
             let callee =  match lookup_function callee the_module with
                             | Some callee -> callee
                             | None -> raise (Error "unknown function referenced")
             in
-            let args = Array.map codegen_expr args in
-            build_call callee args "calltmp" builder
+            let args = List.map codegen_expr args in
+            build_call callee (Array.of_list args) "calltmp" builder
     | BinaryOperator (lhs, op, rhs) ->
             let lhs_val = codegen_expr lhs in
             let rhs_val = codegen_expr rhs in
@@ -28,6 +30,7 @@ let codegen_expr (e: Expr) =
                     | "+" -> build_add lhs_val rhs_val "addtmp" builder
                     | "-" -> build_sub lhs_val rhs_val "subtmp" builder
                     | "*" -> build_mul lhs_val rhs_val "multmp" builder
+                    | _ -> failwith "oof"
             )
     | Identifier name ->
             let v = try Hashtbl.find named_values name with
@@ -42,14 +45,14 @@ let codegen_statement (s: statement) =
     | Expr e -> codegen_expr e
     | VariableDeclarationExpr (t, n, e) -> 
             let expr_val = codegen_expr e in
-            Hashtbl.add named_values n e;
-            e
+            Hashtbl.add named_values n expr_val;
+            expr_val
     | VariableDeclaration (t, n) -> 
-            let expr_val = codegen_expr Int(0) in
-            Hashtbl.add named_values n e;
-            e
-    | FunctionDeclaration (t, n, args, code) -> codegen_expr code
-    _ -> failwith "oof"
+            let expr_val = codegen_expr (Int(0)) in
+            Hashtbl.add named_values n expr_val;
+            expr_val
+    (*| FunctionDeclaration (t, n, args, code) -> codegen_expr code*)
+    | _ -> failwith "oof"
 
 
 
